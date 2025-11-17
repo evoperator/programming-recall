9f05763ff0408f1045f153739d52df39e4c16a20
Operator <131405928+evoperator@users.noreply.github.com>
2025-11-17 22:19:08 +0100

    One more task

--- python/hackerrank/algorithms/easy_algos.py
+++ python/hackerrank/algorithms/easy_algos.py
@@ -351,3 +351,49 @@ if __name__ == '__main__':
 
     fptr.close()
 """
+
+# 11. Grading Students
+"""#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'gradingStudents' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts INTEGER_ARRAY grades as parameter.
+#
+
+def gradingStudents(grades):
+    for i in range(len(grades)):
+        if grades[i] > 37 and grades[i] % 5 != 0:
+            tmp = 5 - grades[i] % 10
+            if tmp < 0:
+                tmp += 5
+            tmp += grades[i]
+            if tmp - grades[i] < 3:
+                grades[i] = tmp
+    return grades
+    
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    grades_count = int(input().strip())
+
+    grades = []
+
+    for _ in range(grades_count):
+        grades_item = int(input().strip())
+        grades.append(grades_item)
+
+    result = gradingStudents(grades)
+
+    fptr.write('\n'.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
+"""
