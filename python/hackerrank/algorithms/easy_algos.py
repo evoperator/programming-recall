416e1a3ce164c02d7755bc0d60d251362db6bc10
Operator <131405928+evoperator@users.noreply.github.com>
2025-11-20 23:06:13 +0100

    Another task

--- python/hackerrank/algorithms/easy_algos.py
+++ python/hackerrank/algorithms/easy_algos.py
@@ -245,3 +245,20 @@ def countApplesAndOranges(s, t, a, b, apples, oranges):
     print(ad)
     print(od)
 """
+
+# 13. Number Line Jumps
+"""
+#
+# Complete the 'kangaroo' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts following parameters:
+#  1. INTEGER x1
+#  2. INTEGER v1
+#  3. INTEGER x2
+#  4. INTEGER v2
+#
+
+def kangaroo(x1, v1, x2, v2):
+    return 'YES' if (v1 > v2 and ((x2 - x1) % (v1 - v2)) == 0) or (x1 == x2) else 'NO'
+"""
