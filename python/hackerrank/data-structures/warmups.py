cba807da27a696881ee22a9eea09056539897df3
Operator <131405928+evoperator@users.noreply.github.com>
2025-11-21 17:01:40 +0100

    Another practice from HackerRank
    Adding tasks about data structures

---
+++ python/hackerrank/data-structures/warmups.py
@@ -0,0 +1,15 @@
+# "Easy" tasks about "data structures" on HackerRank in one file
+
+# Arrays
+# 1. Array - DS
+"""
+#
+# Complete the 'reverseArray' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts INTEGER_ARRAY a as parameter.
+#
+
+def reverseArray(a):
+    return [a[i] for i in range(len(a)-1, -1, -1)]
+"""
