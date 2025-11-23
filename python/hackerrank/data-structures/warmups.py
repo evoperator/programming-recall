69e5aea33bd22e20c39c23fb47d097ab73595e42
Operator <131405928+evoperator@users.noreply.github.com>
2025-11-23 12:11:31 +0100

    Add another task

--- python/hackerrank/data-structures/warmups.py
+++ python/hackerrank/data-structures/warmups.py
@@ -13,3 +13,22 @@
 def reverseArray(a):
     return [a[i] for i in range(len(a)-1, -1, -1)]
 """
+
+# 2. 2D Array - DS
+"""
+#
+# Complete the 'hourglassSum' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts 2D_INTEGER_ARRAY arr as parameter.
+#
+
+def hourglassSum(arr):
+    max_sum = arr[1][1] + arr[0][0] + arr[0][1] + arr[0][2] + arr[2][0] + arr[2][1] + arr[2][2]
+    for i in range(1, len(arr)-1):
+        for j in range(1, len(arr)-1):
+            hourglass = arr[i][j] + arr[i-1][j-1] + arr[i-1][j] + arr[i-1][j+1] + arr[i+1][j-1] + arr[i+1][j] + arr[i+1][j+1]
+            if hourglass > max_sum:
+                max_sum = hourglass
+    return max_sum
+"""
